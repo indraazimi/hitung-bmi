4b4a3a88f648151f1301ef696f5ec6a99bfbe7c9
Indra Azimi <indraazimi@yahoo.com>
2021-02-25 11:41:27 +0700

    Menambahkan view binding

--- app/src/main/java/com/indraazimi/hitungbmi/MainActivity.kt
+++ app/src/main/java/com/indraazimi/hitungbmi/MainActivity.kt
@@ -10,11 +10,23 @@
 package com.indraazimi.hitungbmi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.indraazimi.hitungbmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener { hitungBmi() }
+    }
+
+    private fun hitungBmi() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
