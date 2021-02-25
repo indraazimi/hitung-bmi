dc22f3524f228748c8bcad2754bf4bed2ef5cbe5
Indra Azimi <indraazimi@yahoo.com>
2021-02-25 12:10:04 +0700

    Membuat perhitungan BMI

--- app/src/main/java/com/indraazimi/hitungbmi/MainActivity.kt
+++ app/src/main/java/com/indraazimi/hitungbmi/MainActivity.kt
@@ -10,7 +10,6 @@
 package com.indraazimi.hitungbmi
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.indraazimi.hitungbmi.databinding.ActivityMainBinding
 
@@ -27,6 +26,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hitungBmi() {
-        Log.d("MainActivity", "Tombol diklik!")
+        val berat = binding.beratEditText.text.toString().toFloat()
+        val tinggi = binding.tinggiEditText.text.toString().toFloat() / 100
+        val bmi = berat / (tinggi * tinggi)
+
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        val isMale = selectedId == R.id.priaRadioButton
+        val kategori = getKategori(bmi, isMale)
+
+        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
+        binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
+    }
+
+    private fun getKategori(bmi: Float, isMale: Boolean): String {
+        val stringRes = if (isMale) {
+            when {
+                bmi < 20.5 -> R.string.kurus
+                bmi >= 27.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        } else {
+            when {
+                bmi < 18.5 -> R.string.kurus
+                bmi >= 25.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        }
+        return getString(stringRes)
     }
 }
