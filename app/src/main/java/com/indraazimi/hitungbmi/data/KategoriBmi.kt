e61d963fe82983731125f71ba051af5bb9f03000
Indra Azimi <indraazimi@yahoo.com>
2021-03-08 10:52:04 +0700

    Membuat fragment lihat saran

---
+++ app/src/main/java/com/indraazimi/hitungbmi/data/KategoriBmi.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.hitungbmi.data
+
+enum class KategoriBmi { KURUS, IDEAL, GEMUK }
---
+++ app/src/main/java/com/indraazimi/hitungbmi/ui/SaranFragment.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2021 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.hitungbmi.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.indraazimi.hitungbmi.R
+import com.indraazimi.hitungbmi.data.KategoriBmi
+import com.indraazimi.hitungbmi.databinding.FragmentSaranBinding
+
+class SaranFragment : Fragment() {
+
+    private lateinit var binding: FragmentSaranBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentSaranBinding.inflate(layoutInflater, container, false)
+        updateUI(KategoriBmi.KURUS)
+        return binding.root
+    }
+
+    private fun updateUI(kategori: KategoriBmi) {
+        val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+        when (kategori) {
+            KategoriBmi.KURUS -> {
+                actionBar?.title = getString(R.string.judul_kurus)
+                binding.imageView.setImageResource(R.drawable.kurus)
+                binding.textView.text = getString(R.string.saran_kurus)
+            }
+            KategoriBmi.IDEAL -> {
+                actionBar?.title = getString(R.string.judul_ideal)
+                binding.imageView.setImageResource(R.drawable.ideal)
+                binding.textView.text = getString(R.string.saran_ideal)
+            }
+            KategoriBmi.GEMUK -> {
+                actionBar?.title = getString(R.string.judul_gemuk)
+                binding.imageView.setImageResource(R.drawable.gemuk)
+                binding.textView.text = getString(R.string.saran_gemuk)
+            }
+        }
+    }
+}
