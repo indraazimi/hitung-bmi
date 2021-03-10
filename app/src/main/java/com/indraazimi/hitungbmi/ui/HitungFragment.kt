3440be7a789ebac0da6b28595c20f5978d093b11
Indra Azimi <indraazimi@yahoo.com>
2021-03-10 10:45:49 +0700

    Membuat navigasi menggunakan menu

--- app/src/main/java/com/indraazimi/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/com/indraazimi/hitungbmi/ui/HitungFragment.kt
@@ -11,12 +11,11 @@ package com.indraazimi.hitungbmi.ui
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.indraazimi.hitungbmi.R
 import com.indraazimi.hitungbmi.databinding.FragmentHitungBinding
 import com.indraazimi.hitungbmi.data.KategoriBmi
@@ -34,7 +33,21 @@ class HitungFragment : Fragment() {
             view.findNavController().navigate(HitungFragmentDirections
                     .actionHitungFragmentToSaranFragment(kategoriBmi))
         }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun hitungBmi() {
