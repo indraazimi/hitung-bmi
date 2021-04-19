523cc70abd96fccc8d9138bf0d19fd668dc311d8
Indra Azimi <indraazimi@yahoo.com>
2021-04-19 10:43:34 +0700

    Membuat database dengan Room

---
+++ app/src/main/java/com/indraazimi/hitungbmi/db/BmiEntity.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.hitungbmi.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "bmi")
+data class BmiEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0L,
+    var tanggal: Long = System.currentTimeMillis(),
+    var berat: Float,
+    var tinggi: Float,
+    var isMale: Boolean
+)
